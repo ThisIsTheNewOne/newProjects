478d308064056bfba732c32e692181a850fad99a
Paul Balta <baltapaul2@gmail.com>
2024-12-15 00:12:13 +0200

    adding the artists JSON

--- ArtGalleryProject-Layout/src/App.tsx
+++ ArtGalleryProject-Layout/src/App.tsx
@@ -1,12 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-// import Hello from './components/Hello'
 import Menu from './components/Menu'
 
-function App() {
-  // const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
